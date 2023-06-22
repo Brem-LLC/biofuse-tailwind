0ed62179beb2a681b28ad324392f1e6f8505c94f
Muhammad Khalil <mkhalil7868@gmail.com>
2023-06-22 17:54:34 +0500

    update blog and other page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",'node_modules/preline/dist/*.js',],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {
-      fontFamily: {
-      },
+      fontFamily: {},
       backgroundImage: {
-        polygon: 'linear-gradient(to right, rgba(0,0,0,.25),rgba(0,0,0,.3) , rgba(0,0,0,.2)),url(/src/assets/images/polygons.png)'
+        polygon: "linear-gradient(to right, rgba(0,0,0,.25),rgba(0,0,0,.3) , rgba(0,0,0,.2)),url(/src/assets/images/polygons.png)",
       },
       colors: {
         primary: {
-          DEFAULT: '#45a9e8'
-        }
+          DEFAULT: "#45a9e8",
+        },
       },
     },
   },
-  plugins: [
-    require('preline/plugin')
-  ],
+  plugins: [require("preline/plugin"), require("@tailwindcss/typography")],
 };
