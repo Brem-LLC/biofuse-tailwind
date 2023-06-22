242bf0b6ab9593dbaaafecad8992223301921792
Ted Martin <etmartinkazoo@gmail.com>
2023-06-21 22:59:54 -0400

    Initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",'node_modules/preline/dist/*.js',],
+  theme: {
+    extend: {
+      fontFamily: {
+      },
+      backgroundImage: {
+        polygon: 'linear-gradient(to right, rgba(0,0,0,.25),rgba(0,0,0,.3) , rgba(0,0,0,.2)),url(/src/assets/images/polygons.png)'
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#45a9e8'
+        }
+      },
+    },
+  },
+  plugins: [
+    require('preline/plugin')
+  ],
+};
